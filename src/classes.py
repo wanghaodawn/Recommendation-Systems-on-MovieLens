1a7eb00acd6064d1c198aee80ff413a97cf775d1
Dawn <wanghaodawn@cmu.edu>
2017-06-24 12:02:36 +0800

    Added the class to store data

---
+++ src/classes.py
@@ -0,0 +1,28 @@
+class Link:
+    def __init__(self, movie_id, imdb_id, tmdb_id):
+        self.movie_id = movie_id
+        self.imdb_id = imdb_id
+        self.tmdb_id = tmdb_id
+
+
+class Movie:
+    def __init__(self, movie_id, title, genres):
+        self.movie_id = movie_id
+        self.title = title
+        self.genres = genres
+
+
+class Rating:
+    def __init__(self, user_id, movie_id, rating, timestamp):
+        self.user_id = user_id
+        self.movie_id = movie_id
+        self.rating = rating
+        self.timestamp = timestamp
+
+
+class Tag:
+    def __init__(self, user_id, movie_id, tag, timestamp):
+        self.user_id = user_id
+        self.movie_id = movie_id
+        self.tag = tag
+        self.timestamp = timestamp
